25f73649668f7551dfe1e38b9710c57cdcdcb3aa
MagmaTart <magmatart@gmail.com>
2019-12-10 04:29:40 -0800

    Chapter 12 : bufio 내용 삭제

--- 12. File IO/main.go
+++ 12. File IO/main.go
@@ -51,46 +51,11 @@ func file_write_test() error {
 	return err
 }
 
-func file_bufio_read_test() error {
-	filename := "test.txt"
-	file, err := os.Open(filename)
-	if err != nil {		// 에러 발생시
-		return err
-	}
-	defer file.Close()
-
-	reader := bufio.NewReader(file)
-	if read_str, err := reader.ReadString('\n'); err == nil {
-		fmt.Println(read_str)
-	} else {
-		return err
-	}
-	return err
-}
-
-func file_bufio_write_test() error {
-	filename := "new.txt"
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	writer := bufio.NewWriter(file)
-	if _, err := writer.WriteString("bufio writing test!\n"); err != nil {
-		return err
-	}
-	writer.Flush()
-	return err
-}
-
 func main() {
 	// stdout_test()
 	// stdin_add_test()
 	// sscanf_test()
 	// err := file_read_test()
-	// err := file_write_test()
-	// err := file_bufio_read_test()
-	err := file_bufio_write_test()
+	err := file_write_test()
 	fmt.Println(err)
 }
