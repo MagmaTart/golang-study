e7ae626ce9e4dd7b34d54ccbea0e5982fecd608b
MagmaTart <magmatart@gmail.com>
2019-12-10 04:29:56 -0800

    Chapter 13 : bufio 패키지 작성중

---
+++ 13. bufio/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+import "os"
+import "bufio"
+
+func file_bufio_read_test() error {
+	filename := "test.txt"
+	file, err := os.Open(filename)
+	if err != nil {		// 에러 발생시
+		return err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	if read_str, err := reader.ReadString('\n'); err == nil {
+		fmt.Println(read_str)
+	} else {
+		return err
+	}
+	return err
+}
+
+func file_bufio_write_test() error {
+	filename := "new.txt"
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	if _, err := writer.WriteString("bufio writing test!\n"); err != nil {
+		return err
+	}
+	writer.Flush()
+	return err
+}
+
+func main() {
+	err := file_bufio_read_test()
+	err = file_bufio_write_test()
+	fmt.Println(err)
+}
