411efc607cb150aac814b33c6a45d5a528b3597e
MagmaTart <magmatart@gmail.com>
2019-12-12 05:34:55 -0800

    Chapter 14 : Function 1 작성중

--- 14. Function 1/main.go
+++ 14. Function 1/main.go
@@ -27,12 +27,26 @@ func slice_by_pointer_test(slc *[]int) {
 	print_slice(*slc)
 }
 
+func add_and_mul(a, b int) (int, int) {
+	return a + b, a * b
+}
+
+func named_add_and_mul(a, b int) (add int, mul int) {
+	add = a + b
+	mul = a * b
+	return
+}
+
 func main() {
-	slc := []int{1, 2, 3, 4, 5}
-	fmt.Printf("Main 1 : ")
-	print_slice(slc)
+	// slc := []int{1, 2, 3, 4, 5}
+	// fmt.Printf("Main 1 : ")
+	// print_slice(slc)
 	// slice_by_value_test(slc)
-	slice_by_pointer_test(&slc)
-	fmt.Printf("Main 2 : ")
-	print_slice(slc)
+	// slice_by_pointer_test(&slc)
+	// fmt.Printf("Main 2 : ")
+	// print_slice(slc)
+
+	// add_result, mul_result := add_and_mul(30, 50)
+	add_result, mul_result := named_add_and_mul(30, 50)
+	fmt.Printf("Add : %d, Mul : %d\n", add_result, mul_result)
 }
