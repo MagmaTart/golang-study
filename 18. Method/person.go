facd13ee3819c29153c41b070a07b47ea6ad093a
MagmaTart <magmatart@gmail.com>
2019-12-18 05:31:14 -0800

    Chapter 18 : Method

--- 18. Method/person.go
+++ 18. Method/person.go
@@ -8,6 +8,11 @@ type Person struct{
 	gender string
 }
 
+type Account struct{
+	Person
+	id, pw string
+}
+
 func (person Person) Print() {
 	fmt.Println("Name is", person.name)
 	fmt.Println("Age is", person.age)
@@ -19,8 +24,14 @@ func (person *Person) change_name(new string) {
 }
 
 func main() {
-	var me = Person{"Lee Soomin", 20, "Male"}
-	me.Print()
-	me.change_name("MagmaTart")
-	me.Print()
+	// var me = Person{"Lee Soomin", 20, "Male"}
+	// me.Print()
+	// me.change_name("MagmaTart")
+	// me.Print()
+
+	var acc Account
+	acc.name = "MagmaTart"
+	acc.age = 20
+	acc.gender = "male"
+	acc.Print()
 }
