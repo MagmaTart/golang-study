95dae3cd0c7b52675e928553851c0a31e497aca2
MagmaTart <magmatart@gmail.com>
2019-12-30 10:26:40 +0900

    Chapter 22 init

--- 22. Toy Project 1/main.go
+++ 22. Toy Project 1/main.go
@@ -1,7 +1,13 @@
 package main
 
 import "fmt"
+import "github.com/nsf/termbox-go"
 
 func main() {
-
+  err := termbox.Init()
+  if err != nil {
+    panic(err)
+  }
+  defer termbox.Close()
+  fmt.Println("Init complete")
 }
