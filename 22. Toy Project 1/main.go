889fdc3906742666a52f5a4e70fb8c0fb58c7ad4
MagmaTart <magmatart@gmail.com>
2019-12-30 10:31:05 +0900

    Termbox-go testing codes

--- 22. Toy Project 1/main.go
+++ 22. Toy Project 1/main.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
 import "github.com/nsf/termbox-go"
 
 func main() {
+  // Termbox testing codes
   err := termbox.Init()
   if err != nil {
     panic(err)
   }
   defer termbox.Close()
-  fmt.Println("Init complete")
+  termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+  termbox.SetCell(10, 10, rune('A'), termbox.ColorBlack, termbox.ColorWhite)
+  termbox.Flush()
+  for {}
 }
